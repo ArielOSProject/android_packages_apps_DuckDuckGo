72701bedd13ddb41fc046f9ec34dace309573ee6
Sava Mikalacki <mikalackis@gmail.com>
2018-08-18 17:21:53 +0200

    Initial import

---
+++ Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := DuckDuckGo
+
+LOCAL_CERTIFICATE := platform
+
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_MODULE_CLASS := APPS
+
+LOCAL_SRC_FILES := duckduckgo-5.9.1-release-unsigned.apk
+
+include $(BUILD_PREBUILT)
